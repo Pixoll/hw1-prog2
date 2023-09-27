ad2cc27ba7ffd08c77358554d7811b993dadc226
Pixoll <pixoll.gamer@gmail.com>
2023-09-27 11:00:23 -0300

    Creadas clases Cliente y Articulo

---
+++ src/Articulo.java
@@ -0,0 +1,48 @@
+public class Articulo {
+    private float peso;
+    private String nombre;
+    private String descripcion;
+    private float precio;
+
+    public Articulo() {
+    }
+
+    public float getPeso() {
+        return this.peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public float getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String toString() {
+        return "Articulo" + this.nombre
+                + " (" + this.descripcion + ")"
+                + " Peso: " + this.peso
+                + " Precio" + this.precio;
+    }
+}
---
+++ src/Cliente.java
@@ -0,0 +1,28 @@
+public class Cliente {
+    private String nombre;
+    private String rut;
+
+    public Cliente() {
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getRut() {
+        return this.rut;
+    }
+
+    public void setRut(String rut) {
+        this.rut = rut;
+    }
+
+    public String toString() {
+        return "Cliente " + this.nombre
+                + " Rut: " + this.rut;
+    }
+}
